a607acdbb2291c7038e69e8906555aefaa2d9324
sdorms <stephendorman89@gmail.com>
2026-02-19 00:40:25 -0300

    Copy changes + first blog post

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="font-pixel text-3xl leading-9 font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Stuff I'm learning
+            Stuff
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
